0d8cfad18461159ae8b58e41545063dcee107cca
Sakhman <68939201+Sakhman@users.noreply.github.com>
2020-11-11 10:26:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ g="https://discord.gg/rsQUHwq"
  
 oot_channel_id_list = ["775945251340156930"]
 
-answer_pattern = re.compile(r'(not|n)?([1-3]{1})(\?)?(cnf)?(\?)?$', re.IGNORECASE)
+answer_pattern = re.compile(r'(n|not)?([1-3]{1})(\?)?(cnf|c|cf|conf|apg)?(\w|\ww)?$', re.IGNORECASE)
 
 apgscore = 1000
 nomarkscore = 320
