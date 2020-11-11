db64b0f6b10506c5dd6549be0c5e7c0ce811da4b
Sakhman <68939201+Sakhman@users.noreply.github.com>
2020-11-11 11:52:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ class Bot(discord.Client):
         self.embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/775384878942257173/775933617943347230/unnamed.gif")
         self.embed.set_footer(text='Made by Subrata#3297',icon_url='https://cdn.discordapp.com/avatars/660337342032248832/828f7b13ce161e8a9d4c129e0ac776c4.webp?size=1024')
         self.embed.add_field(name="**__Correct Answer__**", value="0", inline=False)
-        self.embed.add_field(name="**__Not Answer__**", value="0", inline=False) 
+        #self.embed.add_field(name="**__Not Answer__**", value="0", inline=False) 
 
 
         #await self.bot.add_reaction(embed,':spy:')
@@ -157,7 +157,7 @@ class Bot(discord.Client):
         one_cross =""
         two_cross =""
         three_cross =""
-        best_answer = "** **   **`Fetching ...`** ** **"
+        best_answer = "**Option ➜** <a:redload:772439692411011073>"
         not_answer = "** **  **`Erasing ...` ** ** **"
               
 
@@ -165,7 +165,7 @@ class Bot(discord.Client):
         
 
         highest = max(lst_scores)
-        gif_ans = 'https://cdn.discordapp.com/attachments/716879425655799858/745718978214625410/726164501543518278.gif'
+        gif_ans = 'https://cdn.discordapp.com/attachments/769445612231720960/774230429023993896/unnamed.gif'
         best_answer = 'Loading'
         lowest = min(lst_scores)
         answer = lst_scores.index(highest)+1
@@ -177,7 +177,7 @@ class Bot(discord.Client):
                 one_check = " <:emoji_13:772843132093202443>  "
                 mark_check_one = "<:emoji_62:735102374523306047>"
                 gif_ans = "https://cdn.discordapp.com/attachments/716879425655799858/742730340744822864/723415445167931452.png"
-                best_answer = "** ** **Answer :** :one: = ✅"
+                best_answer = "**Option ➜** <:emoji_39:773917426835521536>"
                    
             else:
                 one_check = " "
@@ -186,7 +186,7 @@ class Bot(discord.Client):
                 two_check = " <:emoji_13:772843132093202443>  "
                 mark_check_two = "<:emoji_62:735102374523306047>"
                 gif_ans = "https://cdn.discordapp.com/attachments/716879425655799858/742730400706854972/723416002666299433.png"
-                best_answer = "** ** **Answer :** :two: = ✅"
+                best_answer = "**Option ➜** <:emoji_50:773917478723125269>"
                    
             else:
                 two_check = ""
@@ -195,7 +195,7 @@ class Bot(discord.Client):
                 three_check = " <:emoji_13:772843132093202443> "
                 mark_check_three = "<:emoji_62:735102374523306047>"
                 gif_ans = "https://cdn.discordapp.com/attachments/716879425655799858/742730625647247501/723418348834258974.png"
-                best_answer = "** ** **Answer :** :three: = ✅"
+                best_answer = "**Option ➜** <:emoji_44:773918363402371074>"
                    
             else:
                 three_check = ""
@@ -221,7 +221,7 @@ class Bot(discord.Client):
         self.embed.set_field_at(2, name="**__Option ❸__**", value=f"**[{lst_scores[2]}]({g}){three_check}{three_cross}**")
         self.embed.set_thumbnail(url="{}".format(gif_ans))
         self.embed.set_field_at(3, name="**__Correct Answer__**", value=best_answer, inline=True)
-        self.embed.set_field_at(4, name="**__Not Answer__**", value=not_answer, inline=True) 
+        #self.embed.set_field_at(4, name="**__Not Answer__**", value=not_answer, inline=True) 
 
 
         if self.embed_msg is not None:
@@ -294,7 +294,7 @@ def selfbot_process(update_event, answer_scores):
     selfbot = SelfBot(update_event, answer_scores)
 
     loop = asyncio.get_event_loop()
-    loop.create_task(selfbot.start('NjYwMzM3MzQyMDMyMjQ4ODMy.X4xc9g.XML7C0hxrePba_yvU5zapQi3e6U',
+    loop.create_task(selfbot.start('NjYwMzM3MzQyMDMyMjQ4ODMy.X6uAMA.Z5E4mok4Fn7iAzMRRN_1dLZSGqY',
                                    bot=False))
     loop.run_forever()
 
